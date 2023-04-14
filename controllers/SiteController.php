eeff575b4a617cd725e922a9543f0fbfd81d4729
MagomedLorsanov <lorsanov94@mail.ru>
2023-04-14 11:28:57 +0300

    views,controllers,runtime ..

---
+++ controllers/SiteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+class SiteController 
+{
+    public function contact()
+    {
+        return 'data successfully sent';
+    }
+
+    public function handleContact()
+    {
+        return 'data successfully sent';
+    }
+}
