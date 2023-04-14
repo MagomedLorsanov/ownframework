176e9102e6ccc1353ecc1fb0b1770ef1211d4ce4
MagomedLorsanov <lorsanov94@mail.ru>
2023-04-14 15:59:35 +0300

    passing params to view

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@ use app\core\Application;
 use app\controllers\SiteController;
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
-$app->router->get('/contact', 'contact');
+$app->router->get('/',[SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class,'contact']);
 
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
 
